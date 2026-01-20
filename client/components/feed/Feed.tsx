3e86a428b02e437d1958d39870cb8efbdb44cbb7
AbrarShahriar <AbrarShahriar321@gmail.com>
2026-01-20 17:57:32 +0600

    fix: added backend url in env file, added default sources back in server

--- client/components/feed/Feed.tsx
+++ client/components/feed/Feed.tsx
@@ -26,7 +26,7 @@ export default function FeedPage() {
 
   useEffect(() => {
     const getData = async () => {
-      const res = await fetch("http://localhost:5000/feed");
+      const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/feed`);
       const data = await res.json();
       console.log(data);
 
