22216b4a7846246abb12295b57a73e4c93367dc0
AbrarShahriar <AbrarShahriar321@gmail.com>
2026-01-28 00:33:03 +0600

    fix: removed unnecessary imports

--- client/components/feed/Feed.tsx
+++ client/components/feed/Feed.tsx
@@ -1,8 +1,5 @@
-import { ChevronLeft, ChevronRight } from "lucide-react";
-import { Button } from "../ui/button";
 import FeedList from "./FeedList";
 import { FeedItem } from "@/lib/types";
-import Link from "next/link";
 import FeedPages from "./FeedPages";
 
 interface Props {
